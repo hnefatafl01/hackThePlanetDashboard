195ba01b676bc2840e564396c0da1170da421794
Austin Edwards <austintedwards@gmail.com>
2017-03-04 14:36:23 -0700

    new api

--- app.js
+++ app.js
@@ -17,8 +17,8 @@
         dat.$onInit = onInit;
         dat.gps = gps;
         dat.locationSearch = locationSearch;
-        dat.airPollution = airPollution
-        dat.airTemp = airTemp
+        dat.airPollution = airPollution;
+        dat.airTemp = airTemp;
 
         function onInit() {
 
@@ -70,11 +70,30 @@
         }
 
         function airTemp(local){
-          $http.get("https://api.planetos.com/v1/datasets/noaa_gfs_global_sflux_0.12d/point?origin=dataset-details&lat="+local.lat+"&apikey=97abc5a3bba147dca70b034cf008302f&lon="+local.lng+"&_ga=1.229213460.908249478.1488639799")
+          // $http.get("https://api.planetos.com/v1/datasets/noaa_gfs_global_sflux_0.12d/point?origin=dataset-details&lat="+local.lat+"&apikey=97abc5a3bba147dca70b034cf008302f&lon="+local.lng+"&_ga=1.229213460.908249478.1488639799")
+          // .then(function(res){
+          //   let tempK =res.data.entries[5].data.Temperature_surface
+          //   let temp = (9/5)*(tempK-273)+32
+          //   $scope.view.tempSurface={TempSurface:temp}
+          //   let cloudCover = res.data.entries;
+          //   console.log(cloudCover)
+          // })
+
+          $http.get("https://api.wunderground.com/api/6cd1247caa587deb/conditions/q/"+local.lat+","+local.lng+".json")
           .then(function(res){
-            let tempK =res.data.entries[5].data.Temperature_surface
-            let temp = (9/5)*(tempK-273)+32
-            $scope.view.tempSurface={TempSurface:temp}
+            let weather = res.data.current_observation
+            console.log(weather)
+            $scope.view.uv = weather.UV
+            $scope.view.temp = weather.feelslike_f
+            $scope.view.wind = weather.wind_mph
+            $scope.view.wind_direction = weather.wind_dir
+            $scope.view.weather = weather.weather
+            $scope.view.image = weather.icon_url
+            $scope.view.forecast = weather.forecast_url
+
+
+
+
           })
         }
 
