45fd381775ce3c277c1fdce09da226dff02449fc
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-03-04 13:07:20 -0700

    view is creating the rainbow

--- dash/dash.component.js
+++ dash/dash.component.js
@@ -3,15 +3,27 @@
 
     angular.module("app")
         .component("dash", {
-            controller,
+            controller: controller,
             templateUrl: 'dash/dash.html'
         })
 
-    function controller() {
+    function controller($scope) {
         const vm = this;
-
+        console.log($scope);
+        $scope.view={}
         vm.$onInit = function() {
             console.log('sdfgh');
+            // url(img/background.jpg);"
+            $scope.view.rainbow = [
+                "rgba(69, 7, 204, 0.94) 0%",
+                "rgba(104, 82, 153, 0.94) 10%",
+                "rgba(34, 84, 255, 0.94) 30%",
+                "rgba(255, 197, 98, 0.94) 80%",
+                "rgba(204, 107, 7, 0.94) 100%"
+            ];
+            // vm.abstract = "background.jpg";
+            // vm.kelvin = 100;
+            // vm.temperature = (vm.kelvin + 459.67) * 5 / 9;
         };
 
     }
