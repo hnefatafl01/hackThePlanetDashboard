4c61ea6851867a5b51575c6f8bbaad87d198514c
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-03-04 13:31:04 -0700

    Parsed Rainbow to Array of Objects

--- dash/dash.component.js
+++ dash/dash.component.js
@@ -10,20 +10,23 @@
     function controller($scope) {
         const vm = this;
         console.log($scope);
-        $scope.view={}
+        $scope.view = {}
         vm.$onInit = function() {
             console.log('sdfgh');
             // url(img/background.jpg);"
             $scope.view.rainbow = [
-                "rgba(69, 7, 204, 0.94) 0%",
-                "rgba(104, 82, 153, 0.94) 10%",
-                "rgba(34, 84, 255, 0.94) 30%",
-                "rgba(255, 197, 98, 0.94) 80%",
-                "rgba(204, 107, 7, 0.94) 100%"
+              //Each RGB value has to be an integer between 0 and 255
+              //Each A value has to be a floating point decimal between 0 and 1
+              //each PERCENTAGE value has to be a string between 0% and 100%.
+              {r: 69, g: 7, b: 204, a: .94, percentage: "0%"},
+              {r: 104, g: 82, b: 153, a: .94, percentage: "10%"},
+              {r: 34, g: 84, b: 255, a: .94, percentage: "30%"},
+              {r: 255, g: 197, b: 98, a: .94, percentage: "80%"},
+              {r: 204, g: 107, b: 7, a: .94, percentage: "100%"}
             ];
-            // vm.abstract = "background.jpg";
-            // vm.kelvin = 100;
-            // vm.temperature = (vm.kelvin + 459.67) * 5 / 9;
+            vm.abstract = "background.jpg";
+            vm.kelvin = 100;
+            vm.temperature = (vm.kelvin + 459.67) * 5 / 9;
         };
 
     }
